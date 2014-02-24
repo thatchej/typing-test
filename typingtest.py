cfb76ea11007bad9a426305a7118c2f89c0b4204
Jaron Thatcher <thatchej@kec1130-24.engr.oregonstate.edu>
2014-02-23 18:01:02 -0800

    huge improvements - almost works like a typing test now

--- typingtest.py
+++ typingtest.py
@@ -9,24 +9,32 @@ def main():
 
 	text_source = open('google-10000-english.txt', 'r+');
 	words = []
+	user_input = []
+	count = 10
 	lines = text_source.readlines()
+	
 	for x in range(1, 300):
 		words.append(random.choice(lines).replace('\n', ''))
 
 	initialTime = time.time()
 
 	#60 second typing test
+	while((time.time() - initialTime) < 60):
 
-		
-	for word in words[:10]:
-		print word,
-		READ IN INPUT
-		STORE INPUT WORDS IN ARRAY
+		for word in words[count-9:count]:
+			print word,
 
+		print '\n\n'
+		print 'TYPE THE ABOVE WORDS'
+		print '--------------------'
+
+		user_input.append(raw_input())
 		os.system('clear')
 
-		if (time.time() - initialTime) < 60))	
-			break
+		count += 10
+
+	#calculating score
+
 
 	
 
