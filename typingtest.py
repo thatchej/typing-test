94f345fd382c56f27f261ed2e6cb0524ee9cd5da
Aden Petersen <petersad@kec1130-23.engr.oregonstate.edu>
2014-02-23 16:43:14 -0800

    jsalkfdjalkdsf

--- typingtest.py
+++ typingtest.py
@@ -1,6 +1,16 @@
+import time
 import sys
 
+
 def main():
+
+print "TYPING TEST - ALICE IN WONDERLAND"
+
+initialTime = time.clock()
+
+while (time.clock() - initialTime < 60)
+	
+
 
 if __name__ == '__main__':
 	main()
